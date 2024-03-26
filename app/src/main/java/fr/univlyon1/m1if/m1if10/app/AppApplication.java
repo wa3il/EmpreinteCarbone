e53c57e87c067103eba32a1e3798e79e638b7fb7
HASSAN BABIKER Wail <wailhassanbabiker@gmail.com>
2024-03-26 10:31:13 +0100

    initialisation

---
+++ app/src/main/java/fr/univlyon1/m1if/m1if10/app/AppApplication.java
@@ -0,0 +1,13 @@
+package fr.univlyon1.m1if.m1if10.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppApplication.class, args);
+	}
+
+}
---
+++ app/src/test/java/fr/univlyon1/m1if/m1if10/app/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.univlyon1.m1if.m1if10.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
