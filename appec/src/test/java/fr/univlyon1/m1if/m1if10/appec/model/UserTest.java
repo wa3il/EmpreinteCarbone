7190dba0c6ed243ee674a65012172f27832219c2
adrien <adrien.cervantes@etu.univ-lyon1.fr>
2024-04-23 11:05:28 +0200

    Test User 2

--- appec/src/test/java/fr/univlyon1/m1if/m1if10/appec/model/UserTest.java
+++ appec/src/test/java/fr/univlyon1/m1if/m1if10/appec/model/UserTest.java
@@ -2,7 +2,7 @@ package fr.univlyon1.m1if.m1if10.appec.model;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class UserTest {
     private final User user = new User("toto", "password", "Xyz");
