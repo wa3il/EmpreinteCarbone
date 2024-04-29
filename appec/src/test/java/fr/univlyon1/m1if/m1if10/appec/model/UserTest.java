c729889670d9bc06750b726aef7b46ea05de78d4
ryadh <ryadh@MacBook-Pro-de-ryadh.local>
2024-04-30 01:33:50 +0200

    Ajout Test

--- appec/src/test/java/fr/univlyon1/m1if/m1if10/appec/model/UserTest.java
+++ appec/src/test/java/fr/univlyon1/m1if/m1if10/appec/model/UserTest.java
@@ -3,12 +3,35 @@ package fr.univlyon1.m1if.m1if10.appec.model;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-class UserTest {
+
+ class UserTest {
 
     @Test
-     void testGetUsername() {
-        User user = new User("John", "password123", "john123");
+     void testGetAndSetUsername() {
+        User user = new User();
+        user.setUsername("john123");
         assertEquals("john123", user.getUsername());
+    }
+
+    @Test
+     void testGetAndSetName() {
+        User user = new User();
+        user.setName("John");
+        assertEquals("John", user.getName());
+    }
+
+    @Test
+    public void testGetAndSetPassword() {
+        User user = new User();
+        user.setPassword("password123");
+        assertEquals("password123", user.getPassword());
+    }
+
+    @Test
+    public void testGetUid() {
+        User user = new User();
+        user.setuid(1);
+        assertEquals(1, user.getUid());
     }
 
     @Test
